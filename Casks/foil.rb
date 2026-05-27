f5f8c1dacc56874418a335d6baed38ffcf0710cf
neonwatty <jermwatt@gmail.com>
2026-05-27 09:44:12 -0700

    Style Foil cask

--- Casks/foil.rb
+++ Casks/foil.rb
@@ -8,13 +8,13 @@ cask "foil" do
   homepage "https://github.com/mean-weasel/foil"
 
   auto_updates true
-  depends_on macos: ">= :sonoma"
+  depends_on macos: :sonoma
 
   app "Foil.app"
 
   zap trash: [
     "~/Library/Application Support/com.neonwatty.Foil",
-    "~/Library/Preferences/com.neonwatty.Foil.plist",
     "~/Library/Caches/com.neonwatty.Foil",
+    "~/Library/Preferences/com.neonwatty.Foil.plist",
   ]
 end
