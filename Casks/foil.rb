211c99d21745ffdd658a1610463464c2e7a711e6
neonwatty <jermwatt@gmail.com>
2026-05-27 09:40:57 -0700

    Add Foil cask

---
+++ Casks/foil.rb
@@ -0,0 +1,20 @@
+cask "foil" do
+  version "1.12.2"
+  sha256 "39180396a7d29bd43c03165167823f91f4b7358a3937198f155a7eaae30574ad"
+
+  url "https://github.com/mean-weasel/foil/releases/download/v#{version}/Foil-#{version}-macos.dmg"
+  name "Foil"
+  desc "Menu bar speech-to-text transcription with cloud and local providers"
+  homepage "https://github.com/mean-weasel/foil"
+
+  auto_updates true
+  depends_on macos: ">= :sonoma"
+
+  app "Foil.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.neonwatty.Foil",
+    "~/Library/Preferences/com.neonwatty.Foil.plist",
+    "~/Library/Caches/com.neonwatty.Foil",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# Homebrew Foil
+
+Homebrew tap for [Foil](https://github.com/mean-weasel/foil), a macOS menu bar speech-to-text app with cloud and local transcription providers.
+
+```sh
+brew tap mean-weasel/foil https://github.com/mean-weasel/homebrew-foil
+brew install --cask foil
+```
